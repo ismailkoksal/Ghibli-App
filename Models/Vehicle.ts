763bb6f476a5e250ff057121dddf61a2b170f127
Ismail Koksal <ismail@koksal.fr>
2020-02-05 14:23:55 +0100

    Created Vehicle data model

---
+++ Models/Vehicle.ts
@@ -0,0 +1,10 @@
+export interface Vehicle {
+  id: string;
+  name: string;
+  description: string;
+  vehicleClass: string;
+  length: string;
+  pilot: string;
+  films: string;
+  url: string;
+}
