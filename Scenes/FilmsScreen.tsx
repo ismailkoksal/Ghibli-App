db0389929b821ff15e2399d75dab2f1fa96185b0
Ismail Koksal <ismail@koksal.fr>
2020-02-05 12:01:54 +0100

    Show a list of films and on click show his details Show loading when fetching data

--- Scenes/FilmsScreen.tsx
+++ Scenes/FilmsScreen.tsx
@@ -1,13 +1,99 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {Film} from '../Film';
+import {
+  NavigationStackOptions,
+  NavigationStackProp,
+} from 'react-navigation-stack';
 
-export default class FilmsScreen extends React.Component<any, any> {
-  static navigationsOptions = {
+export interface Props {
+  navigation: NavigationStackProp<{filmId: string}>;
+}
+
+interface State {
+  isLoading: boolean;
+  dataSource: Film[];
+}
+
+function getFilmsFromApiAsync() {
+  return fetch('https://ghibliapi.herokuapp.com/films');
+}
+
+function RenderButton({title, onPress}) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default class FilmsScreen extends React.Component<Props, State> {
+  static navigationOptions: NavigationStackOptions = {
     title: 'Films',
   };
 
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: [],
+    };
+  }
+
+  componentDidMount(): void {
+    getFilmsFromApiAsync()
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        });
+      })
+      .catch(error => console.error(error));
+  }
+
   render() {
-    // const {navigate} = this.props.navigation;
-    return <Text>Films screen works !</Text>;
+    const {navigate} = this.props.navigation;
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <View>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => (
+            <RenderButton
+              title={item.title}
+              onPress={() => navigate('FilmDetails', {filmId: item.id})}
+            />
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    maxHeight: 48,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+  },
+  text: {
+    fontSize: 16,
+    color: '#000000de',
+  },
+});
