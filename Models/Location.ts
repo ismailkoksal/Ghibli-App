434b48cf03d57435eba3fda5b58b02fa60c901c8
Ismail Koksal <ismail@koksal.fr>
2020-02-05 14:13:51 +0100

    Created Location data model

---
+++ Models/Location.ts
@@ -0,0 +1,10 @@
+export interface Location {
+  id: string;
+  name: string;
+  climate: string;
+  terrain: string;
+  surfaceWater: string;
+  residents: string[];
+  films: string[];
+  url: string[];
+}
