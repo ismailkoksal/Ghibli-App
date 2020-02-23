f84deb345a04671d5b016d220c1eeb5c4f9e98c4
Ismail Koksal <ismail@koksal.fr>
2020-02-23 18:28:13 +0100

    Create film reducer

---
+++ src/store/film/reducers.ts
@@ -0,0 +1,30 @@
+import {
+  ADD_FILM,
+  DELETE_FILM,
+  FilmActionTypes,
+  MustSeeFilmsState,
+} from './types';
+
+const initialState: MustSeeFilmsState = {
+  mustSeeFilms: [],
+};
+
+export function filmReducer(
+  state = initialState,
+  action: FilmActionTypes,
+): MustSeeFilmsState {
+  switch (action.type) {
+    case ADD_FILM:
+      return {
+        mustSeeFilms: [...state.mustSeeFilms, action.payload],
+      };
+    case DELETE_FILM:
+      return {
+        mustSeeFilms: state.mustSeeFilms.filter(
+          film => film.id !== action.meta.id,
+        ),
+      };
+    default:
+      return state;
+  }
+}
